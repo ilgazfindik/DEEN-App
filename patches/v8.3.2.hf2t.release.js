a1a5ae3fae1e2196a5589c34998bc873c2d7a78f
ilgazfindik <ilgazfindikk@gmail.com>
2026-09-14 01:58:57 +0300

    Remove broken avatar bootstrap from clean wardrobe base

--- patches/v8.3.2.hf2t.release.js
+++ patches/v8.3.2.hf2t.release.js
@@ -1,5 +1,5 @@
 (()=>{
- const VERSION='8.3.6-CLEAN-BASE',REV='836base1';
+ const VERSION='8.3.6-CLEAN-BASE',REV='836base2';
  const tags=`
 <link rel="stylesheet" href="./patches/v8.3.1.css?v=${REV}" data-deen-hf2t-v831-css>
 <link rel="stylesheet" href="./patches/v8.3.2.hf2.css?v=${REV}" data-deen-hf2t-responsive>
@@ -8,7 +8,6 @@
 <link rel="stylesheet" href="./patches/v8.3.2.hf2s.css?v=${REV}" data-deen-hf2t-fit-base-css>
 <link rel="stylesheet" href="./patches/v8.3.2.hf2t.css?v=${REV}" data-deen-hf2t-fit-css>
 <script>window.__deenV832WearApplied=true;<\/script>
-<script src="./patches/v8.3.2.hf2b.bootstrap.js?v=${REV}" data-deen-hf2t-avatar><\/script>
 <script src="./patches/v8.3.2.hf2k.closet.js?v=${REV}" data-deen-hf2t-closet><\/script>
 `;
  window.DEEN_PATCH_V832HF2T=function(html){let out=String(html);if(out.includes('data-deen-hf2t-closet'))return{html:out,version:VERSION,applied:0};out+=tags;return{html:out,version:VERSION,applied:1}};
