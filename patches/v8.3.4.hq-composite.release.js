2cd0f6b4957aef0152ca81e8e65ac396180ec099
ilgazfindik <ilgazfindikk@gmail.com>
2026-09-14 00:59:51 +0300

    Bump DEEN v8.3.4 render hotfix revision

--- patches/v8.3.4.hq-composite.release.js
+++ patches/v8.3.4.hq-composite.release.js
@@ -1,5 +1,5 @@
 (()=>{
- const VERSION='8.3.4-HQ-COMPOSITE',REV='834hq3';
+ const VERSION='8.3.4-HQ-COMPOSITE',REV='834hq4';
  const tags=`
 <link rel="stylesheet" href="./patches/v8.3.4.hq-composite.css?v=${REV}" data-deen-v834-css>
 <script src="./patches/v8.3.4.hq-composite.runtime.js?v=${REV}" data-deen-v834-runtime><\/script>
