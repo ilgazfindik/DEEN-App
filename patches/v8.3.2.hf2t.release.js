272675d4a7fc47247c76234c594c5b5ffd6550af
ilgazfindik <ilgazfindikk@gmail.com>
2026-09-13 11:23:38 +0300

    Add HF2T release loader

---
+++ patches/v8.3.2.hf2t.release.js
@@ -0,0 +1,17 @@
+(()=>{
+ const VERSION='8.3.2-HF2T',REV='832hf2t1';
+ const tags=`
+<link rel="stylesheet" href="./patches/v8.3.1.css?v=${REV}" data-deen-hf2t-v831-css>
+<link rel="stylesheet" href="./patches/v8.3.2.hf2.css?v=${REV}" data-deen-hf2t-responsive>
+<link rel="stylesheet" href="./patches/v8.3.2.hf2k.css?v=${REV}" data-deen-hf2t-body-css>
+<link rel="stylesheet" href="./patches/v8.3.2.hf2r.css?v=${REV}" data-deen-hf2t-head-css>
+<link rel="stylesheet" href="./patches/v8.3.2.hf2s.css?v=${REV}" data-deen-hf2t-fit-base-css>
+<link rel="stylesheet" href="./patches/v8.3.2.hf2t.css?v=${REV}" data-deen-hf2t-fit-css>
+<script>window.__deenV832WearApplied=true;<\/script>
+<script src="./patches/v8.3.2.hf2b.bootstrap.js?v=${REV}" data-deen-hf2t-avatar><\/script>
+<script src="./patches/v8.3.2.hf2k.closet.js?v=${REV}" data-deen-hf2t-closet><\/script>
+<script src="./patches/v8.3.2.hf2s.runtime.js?v=${REV}" data-deen-hf2t-fit-base><\/script>
+<script src="./patches/v8.3.2.hf2t.runtime.js?v=${REV}" data-deen-hf2t-runtime><\/script>
+`;
+ window.DEEN_PATCH_V832HF2T=function(html){let out=String(html);if(out.includes('data-deen-hf2t-runtime'))return{html:out,version:VERSION,applied:0};out+=tags;return{html:out,version:VERSION,applied:1}};
+})();
