42b5077d599c064a7c62d92cd328a42148a03830
ilgazfindik <ilgazfindikk@gmail.com>
2026-09-14 01:56:36 +0300

    Disable legacy HF2S HF2T render runtimes

--- patches/v8.3.2.hf2t.release.js
+++ patches/v8.3.2.hf2t.release.js
@@ -1,5 +1,5 @@
 (()=>{
- const VERSION='8.3.2-HF2T-PERF',REV='832perf1';
+ const VERSION='8.3.6-CLEAN-BASE',REV='836base1';
  const tags=`
 <link rel="stylesheet" href="./patches/v8.3.1.css?v=${REV}" data-deen-hf2t-v831-css>
 <link rel="stylesheet" href="./patches/v8.3.2.hf2.css?v=${REV}" data-deen-hf2t-responsive>
@@ -10,8 +10,6 @@
 <script>window.__deenV832WearApplied=true;<\/script>
 <script src="./patches/v8.3.2.hf2b.bootstrap.js?v=${REV}" data-deen-hf2t-avatar><\/script>
 <script src="./patches/v8.3.2.hf2k.closet.js?v=${REV}" data-deen-hf2t-closet><\/script>
-<script src="./patches/v8.3.2.hf2s.runtime.js?v=${REV}" data-deen-hf2t-fit-base><\/script>
-<script src="./patches/v8.3.2.hf2t.runtime.js?v=${REV}" data-deen-hf2t-runtime><\/script>
 `;
- window.DEEN_PATCH_V832HF2T=function(html){let out=String(html);if(out.includes('data-deen-hf2t-runtime'))return{html:out,version:VERSION,applied:0};out+=tags;return{html:out,version:VERSION,applied:1}};
+ window.DEEN_PATCH_V832HF2T=function(html){let out=String(html);if(out.includes('data-deen-hf2t-closet'))return{html:out,version:VERSION,applied:0};out+=tags;return{html:out,version:VERSION,applied:1}};
 })();
